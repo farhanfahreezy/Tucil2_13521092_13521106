c8514ed1c92bf6e760f4a5366f90392450ebea7c
farhan <mohdfarhanfahrezy@gmail.com>
2023-02-24 15:33:00 +0700

    generate array with range

---
+++ src/generateCoordinate.py
@@ -0,0 +1,25 @@
+import random
+
+def generateCoordinate(n,r):
+    arrayOfCoordinate = [ [0 for i in range(r)] for j in range(n)]
+    for i in range(n):
+        for j in range(r):
+            arrayOfCoordinate[i][j] = random.randint(-1000,1000)
+    return arrayOfCoordinate
+
+def printCoordinate(coordinate):
+    print("(",end="")
+    for i in range(len(coordinate)):
+        if (i != len(coordinate)-1):
+            print(str(coordinate[i])+",",end="")
+        else:
+            print(coordinate[i],end="")
+    print(")",end="")
+
+def printArrayCoordinate(arrayCoordinate):
+    for i in range(len(arrayCoordinate)):
+        printCoordinate(arrayCoordinate[i])
+        print()
+
+printArrayCoordinate(generateCoordinate(10,3))
+        
