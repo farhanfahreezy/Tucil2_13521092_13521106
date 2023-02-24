74ef397aa2e739722597121c12e348f19302e37f
farhan <mohdfarhanfahrezy@gmail.com>
2023-02-24 22:04:47 +0700

    visualizer

---
+++ src/displayCoordinate.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def displayCoordinate(arrayCoordinate, c1, c2):
+    # c1 dan c2 adalah 2 koordinat yang akan digaris
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Masukin semua koordinat ke diagram
+    for i in range(len(arrayCoordinate)):
+        x, y, z = arrayCoordinate[i]
+        ax.scatter(x, y, z, c='b')
+
+    # Buat garis diantara c1 dan c2
+    x1, y1, z1 = c1
+    x2, y2, z2 = c2
+    ax.plot([x1, x2], [y1, y2], [z1, z2], c='r')
+
+    ax.set_xlabel('Sumbu X')
+    ax.set_ylabel('Sumbu Y')
+    ax.set_zlabel('Sumbu Z')
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
 import generateCoordinate as gc
 import bruteForce as bf
+import displayCoordinate as dc
+import time
 
 def mainBF():
     n = int(input("n: "))
     r = int(input("r: "))
     arrayCoordinate = gc.generateCoordinate(n,r)
+
+    start = time.time()*1000
     tempi,tempj = bf.closestPair(arrayCoordinate)
-    gc.printArrayCoordinate(arrayCoordinate)
+    timetaken = time.time()*1000 - start
+
     print("Closest Pair: ",end="")
     gc.printCoordinate(arrayCoordinate[tempi])
     print(" ",end="")
     gc.printCoordinate(arrayCoordinate[tempj])
+    print()
+
+    print("Executed Time:", timetaken,"ms")
+
+    dc.displayCoordinate(arrayCoordinate,arrayCoordinate[tempi],arrayCoordinate[tempj])
 
 mainBF()
